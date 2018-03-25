eff7d00d63d31176b602871800ec3a3603a2e94f
CS70 Coral <cngo@cs.hmc.edu>
2018-03-25 13:09:35 -0700

    add in more

--- cvqda.R
+++ cvqda.R
@@ -19,7 +19,7 @@ for(i in 1:5)
   trainData <- data[-testIndexes, ]
   trainData$pos = factor(trainData$pos) #drop the mixed position
   testData$pos = factor(testData$pos) #drop the mixed position
-  qda.fit = qda(pos~ ppg+ rpg+apg+ftpercent, data = trainData)
+  qda.fit = qda(pos~ ftpercent, data = trainData)
   
   #calculate train error
   qda.predTr=predict(qda.fit , trainData)
@@ -48,5 +48,9 @@ for(i in 1:5)
   errorTest = c(errorTest, cvErrorTe)
 }
 
-plot(errorTest, type="o", col="blue", ylim=c(0,1))
-lines(errorTrain, type="o", col="red")
+print(paste0("Average Training Error Score: ",mean(errorTrain)))
+print(paste0("Average Testing Error Score: ",mean(errorTest)))
+
+
+#avplot(errorTest, type="o", col="blue", ylim=c(0,1))
+#lines(errorTrain, type="o", col="red")
